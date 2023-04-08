1cc74462a578648ad2f9fdb3e9f5815ba075d40a
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:15:37 +0300

    saved

--- hello-react/public/js/app.js
+++ hello-react/public/js/app.js
@@ -3,8 +3,8 @@
 // 1.Yöntem => React - CDN (eksik araçlar) - Babel
 // 2.Yöntem => create-react-app
 
-var template = /*#__PURE__*/React.createElement("h1", {
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "My first react application"); // JSX: Javascript XML
+}, "My first react application"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- hello-react/src/app.js
+++ hello-react/src/app.js
@@ -1,6 +1,17 @@
 // 1.Yöntem => React - CDN (eksik araçlar) - Babel
 // 2.Yöntem => create-react-app
 
-var template = <h1 id="header">My first react application</h1>; // JSX: Javascript XML
+var template = 
+    <div>
+        <h1 id="header">My first react application</h1>
+        <div>Lorem ipsum dolor sit amet.</div>
+        <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+        </ul>
+    </div>; 
+
+
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
