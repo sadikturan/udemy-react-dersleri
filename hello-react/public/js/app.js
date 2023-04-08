ce00d9bf9e4b463df1ae3ce015ff579681f8a266
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:17:28 +0300

    saved

--- hello-react/public/js/app.js
+++ hello-react/public/js/app.js
@@ -1,10 +1,18 @@
 "use strict";
 
-// 1.Yöntem => React - CDN (eksik araçlar) - Babel
-// 2.Yöntem => create-react-app
-
+var p_name = "IPhone 14 Pro";
+var p_price = 45000;
+var product = {
+  name: "IPhone 15",
+  price: 55000
+};
+function formatPrice(product) {
+  return product.price + " TL";
+}
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "My first react application"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
+}, "\xDCr\xFCn Listesi"), /*#__PURE__*/React.createElement("div", {
+  "class": "product-details"
+}, /*#__PURE__*/React.createElement("h2", null, product.name), /*#__PURE__*/React.createElement("p", null, formatPrice(product)), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."))));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- hello-react/src/app.js
+++ hello-react/src/app.js
@@ -1,15 +1,28 @@
-// 1.Yöntem => React - CDN (eksik araçlar) - Babel
-// 2.Yöntem => create-react-app
+
+var p_name = "IPhone 14 Pro";
+var p_price = 45000;
+
+var product = {
+    name: "IPhone 15",
+    price: 55000
+}
+
+function formatPrice(product) {
+    return product.price + " TL";
+}
 
 var template = 
     <div>
-        <h1 id="header">My first react application</h1>
-        <div>Lorem ipsum dolor sit amet.</div>
-        <ul>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-        </ul>
+        <h1 id="header">Ürün Listesi</h1>
+        <div class="product-details">
+            <h2>{ product.name }</h2>
+            <p>{ formatPrice(product) }</p>
+            <ul>
+                <li>Lorem, ipsum.</li>
+                <li>Lorem, ipsum.</li>
+                <li>Lorem, ipsum.</li>
+            </ul>
+        </div>
     </div>; 
 
 
