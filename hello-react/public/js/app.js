f0e819fb0cd4cce97eb4bf467df0bd0dcf065a59
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:23:45 +0300

    saved

--- hello-react/public/js/app.js
+++ hello-react/public/js/app.js
@@ -12,17 +12,42 @@ var products = [{
   price: 60000
 }];
 var selectedProducts = [];
-function addProduct(event, p_name) {
+function selectProduct(event, p_name) {
   console.log(event.target, p_name);
   if (!selectedProducts.includes(p_name)) {
     selectedProducts.push(p_name);
   }
   renderApp();
 }
+function saveProduct(event) {
+  event.preventDefault();
+  var p_name = event.target.elements.p_name.value;
+  var p_price = event.target.elements.p_price.value;
+  var product = {
+    name: p_name,
+    price: p_price
+  };
+  products.push(product);
+  event.target.elements.p_name.value = "";
+  event.target.elements.p_price.value = "";
+  renderApp();
+}
 function renderApp() {
   var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
     id: "header"
-  }, "\xDCr\xFCn Listesi"), /*#__PURE__*/React.createElement("h3", null, "Se\xE7ilen \xDCr\xFCnler: ", selectedProducts.length), products.map(function (product, index) {
+  }, "\xDCr\xFCn Listesi"), /*#__PURE__*/React.createElement("h3", null, "Se\xE7ilen \xDCr\xFCnler: ", selectedProducts.length), /*#__PURE__*/React.createElement("form", {
+    onSubmit: saveProduct
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "p_name",
+    id: "p_name"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "p_price",
+    id: "p_price"
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "\xDCr\xFCn Ekle")), products.map(function (product, index) {
     return /*#__PURE__*/React.createElement("div", {
       className: "product-details",
       key: index
@@ -30,7 +55,7 @@ function renderApp() {
       type: "button",
       id: index,
       onClick: function onClick(event) {
-        return addProduct(event, product.name);
+        return selectProduct(event, product.name);
       }
     }, "Ekle"));
   }));
--- hello-react/src/app.js
+++ hello-react/src/app.js
@@ -18,11 +18,25 @@ var products = [
 
 var selectedProducts = [];
 
-function addProduct(event, p_name) {
+function selectProduct(event, p_name) {
     console.log(event.target, p_name);
     if (!selectedProducts.includes(p_name)){
         selectedProducts.push(p_name);
     } 
+    renderApp();
+}
+
+function saveProduct(event) {
+    event.preventDefault();
+    var p_name = event.target.elements.p_name.value;
+    var p_price = event.target.elements.p_price.value
+    var product = {
+        name: p_name,
+        price: p_price
+    }
+    products.push(product);
+    event.target.elements.p_name.value = "";
+    event.target.elements.p_price.value = "";
     renderApp();
 }
 
@@ -32,12 +46,18 @@ function renderApp() {
         <h1 id="header">Ürün Listesi</h1>
         <h3>Seçilen Ürünler: { selectedProducts.length }</h3>
 
+        <form onSubmit={saveProduct}>
+            <input type="text" name="p_name" id="p_name" />
+            <input type="text" name="p_price" id="p_price" />
+            <button type="submit">Ürün Ekle</button>
+        </form>
+
         {
             products.map((product,index) => (
                 <div className="product-details" key={index}>
                     { <h2> { product.name } </h2>}
                     { product.price }
-                    <button type="button" id={index} onClick={(event) => addProduct(event, product.name)}>Ekle</button>
+                    <button type="button" id={index} onClick={(event) => selectProduct(event, product.name)}>Ekle</button>
                 </div>
             ))
         }
