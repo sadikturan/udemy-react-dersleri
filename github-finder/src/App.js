457e4789825f8df13be433705c5bc63786585779
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:47:29 +0300

    saved

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,16 @@
 import Navbar from './components/Navbar';
+import User from './components/User';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <div className="container mt-3">
+        <User />
+        <User />
+        <User />
+      </div>
+    </div>
   );
 }
 
--- github-finder/src/components/Navbar.js
+++ github-finder/src/components/Navbar.js
@@ -5,9 +5,12 @@ export class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-primary">
-        <a href="/" className="navbar-brand">
-            <i className={ this.props.icon }></i> { this.props.title }
-        </a>
+        <div className="container">
+          <a href="/" className="navbar-brand">
+              <i className={ this.props.icon }></i> { this.props.title }
+            
+          </a>
+        </div>
       </nav>
     )
   }
