917bda3cf61e5ef2f1b4553ab771a187651c4fa1
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:21:00 +0300

    saved

--- hello-react/public/js/app.js
+++ hello-react/public/js/app.js
@@ -1,23 +1,35 @@
 "use strict";
 
-var product = {
+var products = [{
   name: "iphone 15",
   price: 50000,
   description: "iyi telefon",
-  colors: []
-};
+  colors: ["silver", "red", "white", "black"]
+}, {
+  name: "iphone 16",
+  price: 60000,
+  description: "iyi telefon",
+  colors: ["silver", "red", "white"]
+}, {
+  name: "iphone 17",
+  price: 60000,
+  description: "iyi telefon",
+  colors: ["silver", "red", "white"]
+}];
 function formatPrice(product) {
   return /*#__PURE__*/React.createElement("p", null, product.price, " TL");
 }
-function printDescription(product) {
-  if (product.description) {
-    return /*#__PURE__*/React.createElement("p", null, " ", product.description, " ");
-  }
-}
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "\xDCr\xFCn Listesi"), /*#__PURE__*/React.createElement("div", {
-  id: "product-details"
-}, product.name && product.name.length > 3 ? /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ") : /*#__PURE__*/React.createElement("p", null, " \xFCr\xFCn ismi girilmemi\u015F "), product.price && product.price > 0 && formatPrice(product), product.description && /*#__PURE__*/React.createElement("p", null, " ", product.description, " "), product.colors.length > 0 ? /*#__PURE__*/React.createElement("p", null, "renk se\xE7eneklerimiz mevcut") : "", /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."))));
+}, "\xDCr\xFCn Listesi"), products.map(function (product, index) {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "product-details",
+    key: index
+  }, product.name && product.name.length > 3 ? /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ") : /*#__PURE__*/React.createElement("p", null, " \xFCr\xFCn ismi girilmemi\u015F "), product.price && product.price > 0 && formatPrice(product), product.description && /*#__PURE__*/React.createElement("p", null, " ", product.description, " "), product.colors.length > 0 ? /*#__PURE__*/React.createElement("p", null, "renk se\xE7eneklerimiz mevcut") : "", /*#__PURE__*/React.createElement("ul", null, product.colors.map(function (color, index) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: index
+    }, color);
+  })));
+}));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- hello-react/src/app.js
+++ hello-react/src/app.js
@@ -1,34 +1,51 @@
-var product = {
-    name: "iphone 15",
-    price: 50000,
-    description: "iyi telefon",
-    colors: []
-}
+var products = [
+    {
+        name: "iphone 15",
+        price: 50000,
+        description: "iyi telefon",
+        colors: ["silver","red","white","black"]
+    },
+    {
+        name: "iphone 16",
+        price: 60000,
+        description: "iyi telefon",
+        colors: ["silver","red","white"]
+    }
+    ,
+    {
+        name: "iphone 17",
+        price: 60000,
+        description: "iyi telefon",
+        colors: ["silver","red","white"]
+    }
+]
 
 function formatPrice(product) {
     return <p>{ product.price } TL</p>;
 }
 
-function printDescription(product) {
-    if (product.description) {
-        return <p> { product.description } </p>
-    } 
-}
-
 var template = 
     <div>
         <h1 id="header">Ürün Listesi</h1>
-        <div id="product-details">
-            { (product.name && product.name.length>3) ? <h2> { product.name } </h2>: <p> ürün ismi girilmemiş </p> }
-            { (product.price && product.price > 0) && formatPrice(product) }
-            { product.description &&  <p> { product.description } </p>}
-            { product.colors.length > 0 ? <p>renk seçeneklerimiz mevcut</p>:""}
-            <ul>
-                <li>Lorem, ipsum.</li>
-                <li>Lorem, ipsum.</li>
-                <li>Lorem, ipsum.</li>
-            </ul>
-        </div>
+
+        {
+            products.map((product,index) => (
+                <div className="product-details" key={index}>
+                    { (product.name && product.name.length>3) ? <h2> { product.name } </h2>: <p> ürün ismi girilmemiş </p> }
+                    { (product.price && product.price > 0) && formatPrice(product) }
+                    { product.description &&  <p> { product.description } </p>}
+                    { product.colors.length > 0 ? <p>renk seçeneklerimiz mevcut</p>:""}
+                    <ul>
+                        {
+                            product.colors.map((color,index) => (<li key={index}>{ color }</li>))                    
+                        }
+                    </ul>
+                </div>
+            ))
+        }
+
+       
+
     </div>; 
 
 
