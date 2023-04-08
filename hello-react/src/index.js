d90b7aebe22477eabaaea3b742ca1686e182490c
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:34:30 +0300

    saved

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,2 +1,9 @@
-console.log("deneme");
-console.log("deneme");
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+// const template = React.createElement("p", {}, 'Hello World');
+const template = <p>Hello World</p>;
+
+root.render(template);
