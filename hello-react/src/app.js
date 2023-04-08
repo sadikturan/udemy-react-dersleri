02db65245b4ee5b93765be2c00463dfedbe2f6d3
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:42:42 +0300

    saved

--- hello-react/src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom/client';
-import  App from './components/App';
-import './css/main.scss';
-
-var root = ReactDom.createRoot(document.getElementById("root"));
-
-root.render(<App />);
