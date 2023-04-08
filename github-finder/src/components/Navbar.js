d14155c03975d37991dd35d356860c26b03cdf31
“sadikturan” <sadikturan@gmail.com>
2023-04-08 18:46:58 +0300

    saved

--- github-finder/src/components/Navbar.js
+++ github-finder/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-primary">
         <a href="/" className="navbar-brand">
-            <i class={ this.props.icon }></i> { this.props.title }
+            <i className={ this.props.icon }></i> { this.props.title }
         </a>
       </nav>
     )
@@ -14,6 +15,11 @@ export class Navbar extends Component {
 Navbar.defaultProps = {
     icon: "bi bi-github",
     title: "Github Finder"
+}
+
+Navbar.propTypes = {
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
 }
 
 export default Navbar
