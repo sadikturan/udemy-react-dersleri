edf944858ef8fd352fd8270262d004f09366503b
“sadikturan” <sadikturan@gmail.com>
2023-04-08 21:47:19 +0300

    saved

---
+++ react-hooks/src/App.js
@@ -0,0 +1,12 @@
+// class component => state (Component), lifecycle
+// function component + state (hooks) 
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
