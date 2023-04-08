164224e04ef07be33c48a3d9c0db9af43a76dc05
“sadikturan” <sadikturan@gmail.com>
2023-04-08 21:47:52 +0300

    saved

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,40 @@
 // class component => state (Component), lifecycle
 // function component + state (hooks) 
 
-function App() {
+import { useState } from "react";
+
+// import { Component } from "react";
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <p>Butona { this.state.count } kez tıkladınız.</p> 
+//         <button onClick={ () => this.setState({ count: this.state.count + 1 }) }>+1</button>
+//       </>
+//     );
+//   }
+// }
+
+function App(props) {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState(props.text);
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <p>Butona { count } kez tıkladınız.</p> 
+      <p>girilen text: { text }</p>
+      <button onClick={ () => setCount(count + 1) }>+1</button>
+      <button onClick={ () => setCount(count - 1) }>-1</button>
+      <button onClick={ () => setCount(props.count) }>reset</button>
+      <input type="text" value={ text } onChange={ (e) => setText(e.target.value) }/>
+    </>
   );
 }
 
