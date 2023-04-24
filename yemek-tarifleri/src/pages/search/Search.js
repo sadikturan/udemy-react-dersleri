1b20d8348d5bd449484268260f5a5135e4964d4a
“sadikturan” <sadikturan@gmail.com>
2023-04-24 11:31:34 +0300

    arama sonuçları

--- yemek-tarifleri/src/pages/search/Search.js
+++ yemek-tarifleri/src/pages/search/Search.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Search.css';
+import { useSearchParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+import ProductCard from '../../components/ProductCard';
 
 function Search() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+
+  const url = "http://localhost:3000/tarifler?q=" + query;
+  const {data: tarifler, isLoading, error} = useFetch(url);
+
   return (
-    <div>Search</div>
+    <div className="row mt-3">
+      <h2>Aranan Kelime "{query}"</h2>
+      <hr />
+      { isLoading && <div className='alert alert-warning'>Yükleniyor...</div>}
+      { error && <div className='alert alert-danger'>{error}</div>}
+      {
+        tarifler && tarifler.map(tarif => (
+          <ProductCard key={tarif.id} tarif={tarif} />
+        ))
+      }
+    </div>
   )
 }
 
